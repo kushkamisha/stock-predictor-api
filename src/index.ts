2cbe08efd8f9b8002a401d7b0b3ec2ca069d3062
Misha Kushka <kushka.misha@gmail.com>
2022-05-29 16:34:57 +0200

    Basic API

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('pong');
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`\n\nAPI is listening on port ${PORT}\n\n`)
+})
