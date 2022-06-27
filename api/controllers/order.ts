1388c474a2966e9214b8d32d591aa6863bd19dc6
Misha Kushka <kushka.misha@gmail.com>
2022-06-27 09:57:16 +0200

    Finished with the model

--- api/controllers/order.ts
+++ api/controllers/order.ts
@@ -1,22 +1,11 @@
 import { Spot } from '@binance/connector';
-import { left, right } from '@sweet-monads/either';
 import { Request, Response } from 'express';
-import errorsRegistry from '../../errorsRegistry';
 
 const { BINANCE_API_KEY, BINANCE_API_SECRET, ENVIRONMENT } = process.env;
 const baseURL =
   ENVIRONMENT === 'dev' ? 'https://testnet.binance.vision' : 'https://api.binance.com';
 
 const exchange = new Spot(BINANCE_API_KEY || '', BINANCE_API_SECRET || '', { baseURL });
-// // Get account information
-//   // exchange.account().then((response) => exchange.logger.log(response.data))
-//   try {
-//     // @ts-ignore
-//     const res = await exchange.account();
-//     return res;
-//   } catch (err) {
-//     return left({ type: errorsRegistry.binance.order, msg: (err as Error).message });
-//   }
 
 // TODO: use directly API https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC"
 // TODO: use custom http requestor to interact with API
@@ -30,7 +19,6 @@ export const exchangeInfo = async (req: Request, res: Response) => {
     const response = await exchange
       // @ts-ignore
       .exchangeInfo({ symbol });
-    // const response = https://api.binance.com/api/v3/exchangeInfo?symbol=BNBBTC"
     if (response.status === 200) {
       res.status(200).send({ type: 'success', msg: response.data });
       console.log(response.data);
@@ -39,9 +27,7 @@ export const exchangeInfo = async (req: Request, res: Response) => {
       res /* .status(response.status) */
         .send(response.msg);
     }
-    // return left({ type: errorsRegistry.binance.order, msg: response.statusText });
   } catch (err) {
-    // return left({ type: errorsRegistry.binance.order, msg: (err as Error).message });
     console.log(err);
     res.status(500).send((err as Error).message);
   }
@@ -62,9 +48,7 @@ export const newOrder = async (req: Request, res: Response) => {
       // return right(response.data);
     }
     res.status(response.status);
-    // return left({ type: errorsRegistry.binance.order, msg: response.statusText });
   } catch (err) {
-    // return left({ type: errorsRegistry.binance.order, msg: (err as Error).message });
     res.status(500).send((err as Error).message);
   }
 };
--- model/model.ts
+++ model/model.ts
@@ -9,171 +9,190 @@ import scaler from 'minmaxscaler';
 import yahooStockPrices, { StockPrice } from 'yahoo-stock-prices';
 import { Sequential } from '@tensorflow/tfjs';
 
-(async () => {
-  // Load train data
-  const cryptoCurrency = 'BTC';
-  const againstCurrency = 'USD';
-  const stockName = `${cryptoCurrency}-${againstCurrency}`;
+const loadData = async (stockName: string, start: Date, end: Date) => {
+  let data: StockPrice[];
+  const filename = `${stockName} (from ${start.toDateString()} to ${end.toDateString()}).json`;
+  const trainFileName = path.join(__dirname, 'data', filename);
 
-  const trainStart = new Date('2017-01-01');
-  const trainEnd = new Date('2021-01-01');
-
-  let trainData: StockPrice[];
-  const trainFileName = path.join(__dirname, 'data', `${stockName}.json`);
   if (!fs.existsSync(trainFileName)) {
     console.log(`Load from Yahoo Finance: ${stockName} stock data`);
-    trainData = await yahooStockPrices.getHistoricalPrices(
-      trainStart.getMonth(),
-      trainStart.getDay(),
-      trainStart.getFullYear(),
-      trainEnd.getMonth(),
-      trainEnd.getDay(),
-      trainEnd.getFullYear(),
+    data = await yahooStockPrices.getHistoricalPrices(
+      start.getMonth(),
+      start.getDay(),
+      start.getFullYear(),
+      end.getMonth(),
+      end.getDay(),
+      end.getFullYear(),
       stockName,
       '1d'
     );
-    const trainDataJson = JSON.stringify(trainData);
-    fs.writeFileSync(path.join(__dirname, 'data', `${stockName}.json`), trainDataJson);
+    const dataJson = JSON.stringify(data);
+    fs.writeFileSync(trainFileName, dataJson);
+    console.log('The file was successfully saved');
   } else {
     console.log(`Load from presaved file: ${stockName} stock data`);
-    trainData = JSON.parse(fs.readFileSync(trainFileName, 'utf-8')) as StockPrice[];
+    data = JSON.parse(fs.readFileSync(trainFileName, 'utf-8')) as StockPrice[];
   }
-  console.log(`The number of records of ${stockName} is ${trainData.length}`);
-  const closePrices = trainData.map((x) => x.close);
-  console.log(closePrices.slice(10));
+  console.log(`The number of records of ${stockName} is ${data.length}`);
+  return data.map((x) => x.close); // close prices
+};
+
+const prepareData = (predictionDays: number, prices: number[]): [number[][][], number[]] => {
+  // Scale prices
+  const scaledPrices = scaler.fit_transform(prices) as number[];
+
+  const xNormal = []; // [[<predictionDays> days price], [<predictionDays> days price], ...]
+  const y = [];
+
+  for (let i = predictionDays; i < scaledPrices.length; i++) {
+    xNormal.push(scaledPrices.slice(i - predictionDays, i));
+    y.push(scaledPrices[i]);
+  }
+
+  // Reshaping prices
+  const x = [];
+  for (let i = 0; i < xNormal.length; i++) {
+    x.push(xNormal[i].map((close) => [close]));
+  }
+  return [x, y];
+};
+
+const makeTensors = (x: number[][][], y: number[]): [tf.Tensor3D, tf.Tensor1D] => {
+  const xTensor = tf.tensor3d(x);
+  const yTensor = tf.tensor1d(y);
+  return [xTensor, yTensor];
+};
+
+const trainModel = async (xTensor: tf.Tensor3D, yTensor: tf.Tensor1D) => {
+  const model = tf.sequential();
+  model.add(tf.layers.lstm({ units: 50, returnSequences: true, inputShape: [60, 1] }));
+  model.add(tf.layers.dropout({ rate: 0.2 }));
+  model.add(tf.layers.lstm({ units: 50, returnSequences: true }));
+  model.add(tf.layers.dropout({ rate: 0.2 }));
+  model.add(tf.layers.lstm({ units: 50 }));
+  model.add(tf.layers.dropout({ rate: 0.2 }));
+  model.add(tf.layers.dense({ units: 1 }));
+
+  model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+
+  await model.fit(xTensor, yTensor, {
+    epochs: 25,
+    batchSize: 32,
+    callbacks: {
+      onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log?.loss}`),
+    },
+  });
+
+  return model;
+};
+
+const loadDataAndTrainModel = async (
+  stockName: string,
+  predictionDays: number,
+  start: Date,
+  end: Date
+) => {
+  // Load train data
+  const trainPrices = await loadData(stockName, start, end);
 
   // Prepare data
-  const scaledClosePrices = scaler.fit_transform(closePrices) as number[];
-
-  const predictionDays = 60;
-
-  const xTrainNormal = []; // [[60 days price], [60 days price], ...]
-  const yTrain = [];
-
-  for (let i = predictionDays; i < scaledClosePrices.length; i++) {
-    xTrainNormal.push(scaledClosePrices.slice(i - predictionDays, i));
-    yTrain.push(scaledClosePrices[i]);
-  }
-
-  console.log(xTrainNormal.length);
-  console.log(xTrainNormal[0].length);
-
-  // Reshaping the train data
-  const xTrain = [];
-  for (let i = 0; i < xTrainNormal.length; i++) {
-    xTrain.push(xTrainNormal[i].map((x) => [x]));
-  }
-
-  console.log(xTrain.length);
-  console.log(xTrain[0].length);
-  console.log(xTrain[0][0].length);
+  const [xTrain, yTrain] = prepareData(predictionDays, trainPrices);
 
   // Train model
   const modelFileName = path.join(__dirname, 'models', `${stockName}.json`);
   let model: Sequential;
+  const [xTensorTrain, yTensorTrain] = makeTensors(xTrain, yTrain);
 
   if (fs.existsSync(modelFileName)) {
+    // Load trained model from file
     console.log(`The ${stockName} model already exists. Loading it...`);
     model = (await tf.loadLayersModel(`file://${modelFileName}/model.json`)) as Sequential;
-    console.log(model);
   } else {
-    model = tf.sequential();
-    model.add(tf.layers.lstm({ units: 50, returnSequences: true, inputShape: [60, 1] }));
-    model.add(tf.layers.dropout({ rate: 0.2 }));
-    model.add(tf.layers.lstm({ units: 50, returnSequences: true }));
-    model.add(tf.layers.dropout({ rate: 0.2 }));
-    model.add(tf.layers.lstm({ units: 50 }));
-    model.add(tf.layers.dropout({ rate: 0.2 }));
-    model.add(tf.layers.dense({ units: 1 }));
-
-    model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
-
-    const xs = tf.tensor3d(xTrain);
-    console.log({ xsShape: xs.shape });
-    const ys = tf.tensor1d(yTrain);
-    console.log({ ysShape: ys.shape });
-
-    await model.fit(xs, ys, {
-      epochs: 25,
-      batchSize: 32,
-      callbacks: {
-        onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log?.loss}`),
-      },
-    });
-
+    // Train the model & safe to the file
+    model = await trainModel(xTensorTrain, yTensorTrain);
     await model.save(`file://${modelFileName}`);
-    console.log(model);
   }
 
+  return model;
+};
+
+const testModel = async (
+  stockName: string,
+  predictionDays: number,
+  start: Date,
+  end: Date,
+  model: tf.Sequential
+) => {
   // Load stock test data
-
-  const testStart = new Date('2020-01-01');
-  const testEnd = new Date();
-
-  let testData: StockPrice[];
-  const testFileName = path.join(__dirname, 'data', `${stockName}-test.json`);
-  if (!fs.existsSync(testFileName)) {
-    console.log(`Load test data from Yahoo Finance for ${stockName} stock`);
-    testData = await yahooStockPrices.getHistoricalPrices(
-      testStart.getMonth(),
-      testStart.getDay(),
-      testStart.getFullYear(),
-      testEnd.getMonth(),
-      testEnd.getDay(),
-      testEnd.getFullYear(),
-      stockName,
-      '1d'
-    );
-    const testDataJson = JSON.stringify(testData);
-    fs.writeFileSync(testFileName, testDataJson);
-  } else {
-    console.log(`Load test data from presaved file for ${stockName} stock...`);
-    testData = JSON.parse(fs.readFileSync(testFileName, 'utf-8')) as StockPrice[];
-  }
-  console.log(`The number of records of test data for ${stockName} stock is ${testData.length}`);
-
-  const closePricesTest = testData.map((x) => x.close);
-  console.log({ closePricesTest });
-
+  const testPrices = await loadData(stockName, start, end);
   // Prepare data
-  const scaledClosePricesTest = scaler.fit_transform(closePricesTest) as number[];
-
-  const xTestNormal = [];
-  const yTest = [];
-
-  for (let i = predictionDays; i < scaledClosePricesTest.length; i++) {
-    xTestNormal.push(scaledClosePricesTest.slice(i - predictionDays, i));
-    yTest.push(scaledClosePricesTest[i]);
-  }
-
-  console.log(xTestNormal.length);
-  console.log(xTestNormal[0].length);
-
-  // Reshaping the train data
-  const xTest = [];
-  for (let i = 0; i < xTestNormal.length; i++) {
-    xTest.push(xTestNormal[i].map((x) => [x]));
-  }
-
-  console.log(xTest.length);
-  console.log(xTest[0].length);
-  console.log(xTest[0][0].length);
-
-  const tfXTest = tf.tensor3d(xTest);
-  const tfYTest = tf.tensor1d(yTest);
-
-  const tfPredictedPricesScaled = model.predict(tfXTest);
+  const [xTest, yTest] = prepareData(predictionDays, testPrices);
+  const [xTensorTest, yTensorTest] = makeTensors(xTest, yTest);
+  const tfPredictedPricesScaled = model.predict(xTensorTest);
   // @ts-ignore
   const predictedPricesScaled = tfPredictedPricesScaled.dataSync() as number[];
-  console.log(predictedPricesScaled);
-
   const predictedPrices = scaler.inverse_transform(predictedPricesScaled) as number[];
   const yTestNonScaled = scaler.inverse_transform(yTest) as number[];
-  console.log({ testLen: predictedPrices.length });
-  console.log({ yTest });
-  console.log({ predictedPricesScaled });
-  const modelErrorScaled = tf.losses.meanSquaredError(tf.tensor1d(predictedPricesScaled), tfYTest);
+  const modelErrorScaled = tf.losses.meanSquaredError(
+    tf.tensor1d(predictedPricesScaled),
+    yTensorTest
+  );
   modelErrorScaled.print();
   const modelError = tf.losses.meanSquaredError(tf.tensor1d(predictedPrices), yTestNonScaled);
   modelError.print();
-})();
+};
+
+const predictTomorrowPrice = async (
+  end: Date,
+  stockName: string,
+  predictionDays: number,
+  model: tf.Sequential
+) => {
+  const start = new Date();
+  start.setDate(start.getDate() - predictionDays - 2);
+
+  // Load stock test data
+  const prices = await loadData(stockName, start, end);
+
+  // Prepare data
+  const [xTest, yTest] = prepareData(predictionDays, prices);
+  const [xTensorTest] = makeTensors(xTest, yTest);
+
+  const tfPredictedPricesScaled = model.predict(xTensorTest);
+  // @ts-ignore
+  const predictedPricesScaled = tfPredictedPricesScaled.dataSync() as number[];
+
+  const predictedPrices = scaler.inverse_transform(predictedPricesScaled) as number[];
+  // const yTestNonScaled = scaler.inverse_transform(yTest) as number[];
+  return predictedPrices[0]; // tomorrow's price
+};
+
+const pricePredictor = async () => {
+  const cryptoCurrency = 'BTC';
+  const againstCurrency = 'USD';
+  const stockName = `${cryptoCurrency}-${againstCurrency}`;
+  const predictionDays = 60;
+
+  /**
+   * Model training
+   */
+  const trainStart = new Date('2017-01-01');
+  const trainEnd = new Date('2021-01-01');
+  const model = await loadDataAndTrainModel(stockName, predictionDays, trainStart, trainEnd);
+
+  /**
+   * Model testing
+   */
+  // const testStart = new Date('2020-01-01');
+  // const testEnd = new Date();
+  // await testModel(stockName, predictionDays, testStart, testEnd, model);
+
+  /**
+   * Predict tomorrow's price
+   */
+  const today = new Date(); // today
+  const price = await predictTomorrowPrice(today, stockName, predictionDays, model);
+  console.log({ price });
+};
+
+pricePredictor();
